a26b87f251cc1b83d64fdea3a4707ee8a4a63225
Debashis9012 <dedas@thoughtworks.com>
2020-05-09 16:02:37 +0530

    deleted unwanted files and added required files

--- RestSharp/RestSharpDemo/RestSharpDemo/Tests/Tests.cs
+++ RestSharp/RestSharpDemo/RestSharpDemo/Tests/Tests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using RestSharp;
+using RestSharp.Authenticators;
 using RestSharp.Serialization.Json;
 
 namespace RestSharpDemo
@@ -12,38 +13,53 @@ namespace RestSharpDemo
     {
         private RestClient _restClient;
         private RestRequest _restRequest;
-           [SetUp]
+
+        [SetUp]
         public void Setup()
         {
-             _restClient = new RestClient("https://reqres.in");
+            _restClient = new RestClient("https://reqres.in");
         }
-       [Test]
-        public void Test1()
-        { 
+
+        [Test]
+        public void SimpleGetRequest()
+        {
             //Arrange
-            _restRequest = new RestRequest("api/users?page=2",Method.GET);
+            _restRequest = new RestRequest("api/users?page=2", Method.GET);
             //Act
-          var result = _restClient.Execute(_restRequest).Content;
+            var result = _restClient.Execute(_restRequest).Content;
             //Assert
             Console.WriteLine("This is the response:: " + result);
         }
+
         [Test]
         public void VerifyTotalNumOfRecords()
         {
-           // RestClient restClient = new RestClient("https://reqres.in");
-           // RestRequest restRequest = new RestRequest("api/users?page=2",Method.GET);
+            // RestClient restClient = new RestClient("https://reqres.in");
+            // RestRequest restRequest = new RestRequest("api/users?page=2",Method.GET);
             var restResponse = _restClient.Execute(_restRequest);
             //inbuilt deserializer
             var jsonDesrializer = new JsonDeserializer();
-            var output= jsonDesrializer.Deserialize<Dictionary<string,string>>(restResponse);
-           var finaloutput = output["total"];
-           Console.WriteLine("*** The deserialized is:: ***  "+ finaloutput);
+            var output = jsonDesrializer.Deserialize<Dictionary<string, string>>(restResponse);
+            var finaloutput = output["total"];
+            Console.WriteLine("*** The deserialized is:: ***  " + finaloutput);
+           
             //JObject - NewTonSoft
             JObject jobject = JObject.Parse(restResponse.Content);
             var jObjFinalOutput = jobject["total"];
             Console.WriteLine("The deserialized value is::" + jObjFinalOutput);
-            Assert.That(jObjFinalOutput.ToString(), Is.EqualTo("12"),"Total record does not match");
-       }
+            Assert.That(jObjFinalOutput.ToString(), Is.EqualTo("12"), "Total record does not match");
+        }
+
+        [Test]
+        public void GetrequestWithAuth()
+        {
+            _restClient.Authenticator = new HttpBasicAuthenticator("eve.holt@reqres.in", "cityslicka");
+            _restRequest = new RestRequest("https://reqres.in/api/login", Method.GET);
+            var response = _restClient.Execute(_restRequest);
+            Console.WriteLine("content:: " + response.Content);
+            Console.WriteLine("Status code " + (int) response.StatusCode);
+            Assert.That((int) response.StatusCode, Is.EqualTo(200));
+        }
         [TearDown]
         public void close()
         {
